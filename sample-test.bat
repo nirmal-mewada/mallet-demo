e59bdc2a757e596473ed4b0a753186537d5a5fe5
Nirmal <nirmalmewada@gmail.com>
2014-07-31 23:02:53 +0530

    test

--- sample-test.bat
+++ sample-test.bat
@@ -6,6 +6,6 @@ set datadir=sample-data\web\en\
 rem echo output will be redirected to:  %outfile% 
 rem cmd /C "mallet import-dir --input %datadir% --output %datafile% --keep-sequence --remove-stopwords"> %outfile% 
 cmd /C "mallet train-classifier --input %datafile% --trainer MaxEnt --trainer NaiveBayes  --training-portion 0.9 --num-trials 10 --output-classifier classifier/ent-naive.classifier" > %outfile% 
-pause
+pause 
 
 
